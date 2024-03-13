b4b9c20d378f3fb47861eb4c7b88315a49bd818f
olasmartTech <eweleyeolamide10@gmail.com>
2024-03-13 12:02:05 -0700

    set up folder structure and eslint for code formatting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// catch undefined routes
+app.get('*', (req, res) => {
+  return res.send('no such route defined on the server');
+});
+
 app.listen(3000, () => {
   console.log('server running on port 3000');
 });
